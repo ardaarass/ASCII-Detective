fa98fa51179330a3946a96984b170fe3612e6c25
Arda Aras <ardapatu@gmail.com>
2025-11-30 21:48:11 +0300

    Kaynak kod

---
+++ ascıı_dedektifi.rb
@@ -0,0 +1,23 @@
+puts "ASCII yorumlayıcısına hoşgeldiniz,karakter sınıflandırması yapılması için bir girdi bekleniyor..."
+a = gets.chomp
+a = a.ord
+
+if 96 < a&&a < 123
+  puts "LOWER"
+
+elsif 64 < a&&a < 91
+  puts "UPPER"
+
+elsif 47 < a&&a < 65
+  puts "DIGIT"
+
+elsif a == 32
+  puts "SPACE"
+
+elsif 32 < a&&a < 48 || 57 < a&&a < 65 || 90 < a&&a < 97 || 122 < a&&a < 127
+  puts "PUNCT"
+
+else
+  puts "OTHER"
+
+end
